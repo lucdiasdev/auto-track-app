990b5ff9dee71f4c3928fdfe3dbf9e5b0fa5d3ed
Lucas Dias <lucrodrigs@gmail.com>
2025-03-25 16:00:36 -0300

    Initial Commit

---
+++ AutoTrackApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoTrackApp
+//
+//  Created by Lucas Rodrigues Dias on 25/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AutoTrackAppTests/AutoTrackAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutoTrackAppTests.swift
+//  AutoTrackAppTests
+//
+//  Created by Lucas Rodrigues Dias on 25/03/25.
+//
+
+import Testing
+@testable import AutoTrackApp
+
+struct AutoTrackAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
