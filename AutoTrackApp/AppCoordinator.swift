237e23079e29925e62af517d8909518e462d6a87
Lucas Dias <lucrodrigs@gmail.com>
2025-03-26 19:01:15 -0300

    initial commit

---
+++ AutoTrackApp/AppCoordinator.swift
@@ -0,0 +1,23 @@
+//
+//  AppCoordinator.swift
+//  AutoTrackApp
+//
+//  Created by Lucas Rodrigues Dias on 26/03/25.
+//
+
+import Foundation
+import UIKit
+
+class AppCoordinator {
+    
+    let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+        self.window.makeKeyAndVisible()
+    }
+    
+    func startCoordinator() {
+        
+    }
+}
