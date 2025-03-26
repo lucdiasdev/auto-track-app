5929e60287bf8d45e57d588afe22b79de3ba8bb6
Lucas Dias <lucrodrigs@gmail.com>
2025-03-26 19:01:52 -0300

    teste

--- AutoTrackApp/SceneDelegate.swift
+++ AutoTrackApp/SceneDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    auhsuahsuah
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
