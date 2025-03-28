86636a6f50e4a74a71290fc0b0ff3db55281f54c
Lucas Dias <lucrodrigs@gmail.com>
2025-03-28 13:49:19 -0300

    implementa protocolo Coordinator para abstracao na construcao de coordinators

--- AutoTrackApp/Commons/Coordinator.swift
+++ AutoTrackApp/Commons/Coordinator.swift
@@ -8,26 +8,50 @@
 import Foundation
 import UIKit
 
-///user o protocolo "Coordinator" para implementar nos coordinators
-///aqui teremos abstração de alguns pontos que iremos utilizar
-/// /*viewController*/ -> todo inicio do fluxo necessita de uma UIViewController
-/// entao tente sempre quando "pushar" um fluxo segurar a referencia da view controller do mesmo
-/// /*navigationController*/ -> todo fluxo precisa de uma UINavigationController
-/// entao
+/// O `navigationController` gerencia a pilha de navegação da interface.
+/// Ele é responsável por controlar a navegação entre as telas dentro do fluxo do Coordinator.
+/// A boa pratica é sempre mantermos uma navigation durante um fluxo e evitar inicializar outras demais
+/// Por este motivo temos como argumento no start do `Coordinator` uma UINavigation
+///
+/// Exemplo de uso:
+/// ```
+/// func start(using navigationController: UINavigationController)
+/// self.navigationController = navigationController
+/// navigationController.pushViewController(viewController, animated: true)
+/// ```
 
-/**
-    R é um typealias para LocalizableFiles,
-    ele utiliza o NSLocalizedString para administrarmos os textos dentro do aplicativo
- 
-  `LocalizableFiles` Nome do arquivo de string.
- 
-  `key: String` Chave da string.
-  `args: [CVarArg]` Argumentos para substituição de texto Ex: %@.
- 
-  - *Exemplo da utilização da função:*
-  *         R.default.localized(key: "title")
-  *         R.default.localizedWithArgs(key: "error", args: [error.localizedDescription])
- */
+/// /// O `childCoordinator` é um Coordinator filho que pode ser iniciado dentro deste Coordinator.
+/// Ele é útil para dividir responsabilidades e evitar que um único Coordinator fique muito grande.
+/// Passando no argumento do start a navigation que ja esta inicializada
+///
+/// Exemplo de uso:
+/// ```
+/// let childCoordinator = LoginCoordinator()
+/// childCoordinator = childCoordinator
+/// child.start(using: navigationController)
+/// ```
+
+/// O `viewController` é a tela principal gerenciada por este Coordinator.
+/// Ele é responsável por exibir a interface gráfica correspondente ao fluxo que o Coordinator controla.
+/// Geralmente sempre utilizada no construtor/inicializador do coordinator que esta sendo chamado
+/// E assim seguindo o fluxo implementando a `viewController` dentro do `push` no start como mostrado no exemplo de uso da `navigation`
+///
+/// Exemplo de uso:
+/// ```
+/// let loginViewController = LoginViewController(viewModel: LoginViewModel())
+/// self.viewController = loginViewController
+/// ```
+
+/// `start(using navigationController: UINavigationController)`
+/// Método para iniciar o Coordinator, recebendo um `UINavigationController` para controlar a navegação.
+/// - Parameter navigationController: O controlador de navegação onde as telas serão empilhadas.
+///
+/// Exemplo de uso:
+/// ```
+/// let navigation = UINavigationController()
+/// let loginCoordinator = LoginCoordinator()
+/// loginCoordinator.start(using: navigation)
+/// ```
 
 protocol Coordinator {
     var childCoordinator: Coordinator? { get set }
@@ -35,4 +59,6 @@ protocol Coordinator {
     var navigationController: UINavigationController? { get set }
     
     func start(using navigationController: UINavigationController)
+    
+    ///TODO: `func stop()` implementar futuramente um stop para matar as referencias e evitar retain cycle
 }
